388d2fe82509628c79c11db278ba402281e852f9
jackdotwa <jacques@spatialedge.co.za>
2020-02-22 10:49:27 +0200

    initial setup and cleanup of files with addition of build pipeline goodies

---
+++ flattendict/about.py
@@ -0,0 +1,19 @@
+import flattendict.__init__
+
+
+name = "flattendict"
+description = (
+    "Light-weight utility to flatten nested json-like object, into a flat dictionary"
+)
+version = flattendict.__init__.__version__
+author = "jackdotwa"
+email = ""
+uri = "https://github.com/jackdotwa/python-flatten-dict"
+keywords = 'utilities'
+classifiers = [
+    'Environment :: Console',
+    'Intended Audience :: Developers',
+    'License :: Apache2',
+    'Programming Language :: Python :: 3',
+    'Topic :: Utilities'
+]
---
+++ setup.py
@@ -0,0 +1,44 @@
+import os
+from setuptools import find_packages
+from setuptools import setup
+
+base_dir = os.path.abspath(os.path.dirname(__file__))
+os.chdir(base_dir)
+
+# setup place holders for package info that comes from about.py file
+name = None
+description = None
+version = None
+uri = None
+author = None
+email = None
+classifiers = None
+
+with open("flattendict/about.py") as about_file:
+    about = about_file.read()
+    exec(about)
+
+with open('README.md') as readme_file:
+    README = readme_file.read()
+
+with open('requirements.txt') as req:
+    requirements = list(filter(None, req.read().split('\n')))
+
+EXCLUDE_FROM_PACKAGES = ['docs', 'tests*']
+
+setup(
+    name=name,
+    description=description,
+    long_description=README,
+    long_description_content_type="text/markdown",
+    version=version,
+    url=uri,
+    author=author,
+    author_email=email,
+    classifiers=classifiers,
+    packages=find_packages(exclude=EXCLUDE_FROM_PACKAGES),
+    install_requires=requirements,
+    extras_require={},
+    test_suite="tests",
+    include_package_data=True
+)
